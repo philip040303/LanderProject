5a343ebb3fe6f5316f700ec7b72dc392d4683c76
Jim Lampariello <jlampariello@gmail.com>
2022-05-17 19:29:11 -0700

    Including skeleton files for HW2

---
+++ FunctionFile.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def ApogeeRaise(apogeeStart):
+    # this function takes in an apogee and calculates the dV 
+    # necessary to raise the orbit to 410,000 km 
+    
+    # the input value is assumed to be an altitude, so this function
+    # adjusts it by the radius of the earth when doing calculations
+    mu = 
+    apogeeEnd = 410000
+    rEarth = 
+    rStart = rEarth+185
+    aStart = 
+    aEnd   = 
+        
+    vStart = 
+    vEnd   = 
+    dv     = vEnd-vStart
+       
+    return dv*1000 # multiply by 1000 to put in m/s
