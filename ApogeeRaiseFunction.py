2e3174e5c494b326fb3ece08a30bd4524de6be35
Philip Lombardi <plombar3@nd.edu>
2022-05-19 12:48:54 -0400

    Added equations to calculate vStart and vEnd

--- ApogeeRaiseFunction.py
+++ ApogeeRaiseFunction.py
@@ -13,8 +13,8 @@ def ApogeeRaise(apogeeStart):
     aStart = (185+rEarth + apogeeStart+rEarth)/2
     aEnd   = (185+rEarth + apogeeEnd+rEarth)/2
         
-    vStart = 
-    vEnd   = 
+    vStart = np.sqrt(mu*((2/rStart) - (1/aStart)))
+    vEnd   = np.sqrt(mu*((2/rStart) - (1/aEnd)))
     dv     = vEnd-vStart
        
     return dv*1000 # multiply by 1000 to put in m/s
